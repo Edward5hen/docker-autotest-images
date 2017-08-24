2e44eaa639ee23799c9cc74a213fdd9603f31e97
Edward Shen <weshen@redhat.com>
2017-08-24 16:11:40 +0800

    modify sadc related TCs

--- images/sadc/run_options.py
+++ images/sadc/run_options.py
@@ -20,14 +20,15 @@ Prerequisites
 
 *   sadc container is installed
 """
+import time
 
 from autotest.client import utils
 from dockertest.subtest import SubSubtest
 from dockertest.images import DockerImages
-import time
+from sadc import sadc_base
 
 
-class run_options(SubSubtest):
+class run_options(sadc_base):
 
     def initialize(self):
         super(run_options, self).initialize()
@@ -46,29 +47,36 @@ class run_options(SubSubtest):
         self.sub_stuff['env_vir_name'] = ''
         self.sub_stuff['env_vir_image'] = ''
 
+        # Make sure image is loaded and installed
+        self.load_image(self.config['img_stored_location'])
+        self.get_installed()
+
     def list_mounted_dir(self):
         dcr_exec_cmd = 'sudo docker exec sadc-docker '
         etc_sysconfig_sysstat_cmd = 'ls -go /etc/sysconfig/sysstat'
-        self.sub_stuff['etc_sysconfig_sysstat_rst_ctn'] = utils.run(
-                dcr_exec_cmd + etc_sysconfig_sysstat_cmd)
-        self.sub_stuff['etc_sysconfig_sysstat_rst_host'] = utils.run(
-                etc_sysconfig_sysstat_cmd)
+        self.sub_stuff['etc_sysconfig_sysstat_rst_ctn'] = self.format_output(
+            utils.run(dcr_exec_cmd + etc_sysconfig_sysstat_cmd).stdout)
+        self.sub_stuff['etc_sysconfig_sysstat_rst_host'] = self.format_output(
+            utils.run(etc_sysconfig_sysstat_cmd).stdout)
 
         etc_sysconfig_ioconf_cmd = 'ls -go /etc/sysconfig/sysstat.ioconf'
-        self.sub_stuff['etc_sysconfig_ioconf_rst_ctn'] = utils.run(
-                dcr_exec_cmd + etc_sysconfig_ioconf_cmd)
-        self.sub_stuff['etc_sysconfig_ioconf_rst_host'] = utils.run(
-                etc_sysconfig_ioconf_cmd)
+        self.sub_stuff['etc_sysconfig_ioconf_rst_ctn'] = self.format_output(
+            utils.run(dcr_exec_cmd + etc_sysconfig_ioconf_cmd).stdout)
+        self.sub_stuff['etc_sysconfig_ioconf_rst_host'] = self.format_output(
+            utils.run(etc_sysconfig_ioconf_cmd).stdout)
 
         var_log_sa_cmd = 'ls -go /var/log/sa'
-        self.sub_stuff['var_log_sa_rst_ctn'] = utils.run(dcr_exec_cmd +
-                                                         var_log_sa_cmd)
-        self.sub_stuff['var_log_sa_rst_host'] = utils.run(var_log_sa_cmd)
+        self.sub_stuff['var_log_sa_rst_ctn'] = set(self.format_output(
+            utils.run(dcr_exec_cmd + var_log_sa_cmd).stdout))
+        self.sub_stuff['var_log_sa_rst_host'] = set(self.format_output(
+            utils.run(var_log_sa_cmd).stdout))
 
         host_cmd = 'ls -go /host'
         root_cmd = 'ls -go /'
-        self.sub_stuff['host_rst_ctn'] = utils.run(dcr_exec_cmd + host_cmd)
-        self.sub_stuff['root_rst_host'] = utils.run(root_cmd)
+        self.sub_stuff['host_rst_ctn'] = set(self.format_output(
+            utils.run(dcr_exec_cmd + host_cmd).stdout))
+        self.sub_stuff['root_rst_host'] = set(self.format_output(
+            utils.run(root_cmd).stdout))
 
     def echo_env_virs(self):
         dcr_exec_cmd = 'docker exec sadc-docker '
@@ -84,34 +92,36 @@ class run_options(SubSubtest):
         # If testing by loading the image, NAME viriable is sadc-docker
         self.failif(not self.sub_stuff['env_vir_name'].startswith('sadc'),
                     'Env viriable NAME is set wrongly!')
+        self.loginfo('Env NAME is %s' % self.sub_stuff['env_vir_name'])
         # If testing by loading the image, IMAGE viriable is bit complex,
         # but definitely includes *sadc*
         self.failif('sadc' not in self.sub_stuff['env_vir_image'],
                     'Env viriable IMAGE is set wrongly!')
+        self.loginfo('Env IMAGE is %s' % self.sub_stuff['env_vir_image'])
 
     def check_mounted_dir(self):
-        self.failif_ne(self.sub_stuff['etc_sysconfig_sysstat_rst_ctn'].stdout,
-                       self.sub_stuff['etc_sysconfig_sysstat_rst_host'].stdout,
-                       '/etc/sysconfig/sysstat mounted failed!')
-        self.failif_ne(self.sub_stuff['etc_sysconfig_ioconf_rst_ctn'].stdout,
-                       self.sub_stuff['etc_sysconfig_ioconf_rst_host'].stdout,
-                       '/etc/sysconfig/sysstat.ioconf mounted failed!')
-        self.failif_ne(self.sub_stuff['var_log_sa_rst_ctn'].stdout,
-                       self.sub_stuff['var_log_sa_rst_host'].stdout,
-                       '/var/log/sa mounted failed!')
-        self.failif_ne(self.sub_stuff['host_rst_ctn'].stdout,
-                       self.sub_stuff['root_rst_host'].stdout,
-                       '/host mounted failed!')
+        self.failif_ne(self.sub_stuff['etc_sysconfig_sysstat_rst_ctn'],
+                       self.sub_stuff['etc_sysconfig_sysstat_rst_host'],
+                       '/etc/sysconfig/sysstat is mounted failed!')
+        self.loginfo('/etc/sysconfig/sysstat is mounted successfully')
+        self.failif_ne(self.sub_stuff['etc_sysconfig_ioconf_rst_ctn'],
+                       self.sub_stuff['etc_sysconfig_ioconf_rst_host'],
+                       '/etc/sysconfig/sysstat.ioconf is mounted failed!')
+        self.loginfo('/etc/sysconfig/sysstat.ioconf is mounted successfully')
+        self.failif_ne(self.sub_stuff['var_log_sa_rst_ctn'],
+                       self.sub_stuff['var_log_sa_rst_host'],
+                       '/var/log/sa is mounted failed!')
+        self.loginfo('/var/log/sa is mounted successfully')
+        self.failif_ne(self.sub_stuff['host_rst_ctn'],
+                       self.sub_stuff['root_rst_host'],
+                       'root dir is mounted failed!')
+        self.loginfo('root dir is mounted successfully')
 
     def run_once(self):
         """
         Called to run test
         """
         super(run_options, self).run_once()
-        imgs = DockerImages(self)
-        for img_name in imgs.list_imgs_full_name():
-            if 'sadc' in img_name:
-                self.sub_stuff['img_name'] = img_name
         run_cmd = "sudo atomic run %s" % self.sub_stuff['img_name']
         self.sub_stuff['run_result'] = utils.run(run_cmd, timeout=10)
         time.sleep(5)
@@ -124,13 +134,15 @@ class run_options(SubSubtest):
 
     def postprocess(self):
         super(run_options, self).postprocess()
-        # If it's tested by loading the image, container name is rsyslog-docker
-        # If it's tested by pulling the image, container name is rsyslog
+        # If it's tested by loading the image, container name is sadc-docker
+        # If it's tested by pulling the image, container name is sadc
         self.failif_ne(self.sub_stuff['run_result'].exit_status, 0,
                        "image runs failed with atomic commands!")
+        self.loginfo('Image runs successfully')
         self.failif_not_in(self.sub_stuff['ctn_name_rst'].stdout.strip(),
                            ['sadc', 'sadc-docker'],
                            "Container is wrongly named!")
+        self.loginfo('Container name %s' % self.sub_stuff['ctn_name_rst'])
 
         self.check_mounted_dir()
         self.check_env_vir()
--- images/sadc/sadc.py
+++ images/sadc/sadc.py
@@ -18,11 +18,13 @@ Prerequisites
 
 *   Clean host without sadc container ever installed
 """
+import re
 
 from autotest.client import utils
 from dockertest.subtest import SubSubtest
 from dockertest.subtest import SubSubtestCaller
 from dockertest.images import DockerImages
+from autotest.client.shared import error
 
 
 class sadc(SubSubtestCaller):
@@ -30,28 +32,67 @@ class sadc(SubSubtestCaller):
     config_section = 'images/sadc'
 
 
-class load(SubSubtest):
+class sadc_base(SubSubtest):
 
     def initialize(self):
-        super(load, self).initialize()
-        self.sub_stuff['load_result'] = None
+        super(sadc_base, self).initialize()
+        self.sub_stuff['img_name'] = None
+        self.regx = '[0-9]{1,3}'
 
     def load_image(self, location):
-        cmd = 'cat %s | sudo docker load' % location
-        cmd_result = utils.run(cmd, timeout=300, ignore_status=True)
-        return cmd_result
+        if not self.check_loaded():
+            cmd = 'cat %s | sudo docker load' % location
+            utils.run(cmd, timeout=300, ignore_status=True)
+            self.loginfo('Image is loaded successfully')
+            self.check_loaded()
+        else:
+            self.loginfo('Image does not need to load')
 
-    def run_once(self):
-        img_stored_location = self.config['img_stored_location']
-        cmd_result = self.load_image(img_stored_location)
-        self.sub_stuff['load_result'] = cmd_result
+    def check_loaded(self):
+        images = DockerImages(self)
+        cmd = 'sudo docker inspect %s | grep -i "\\"release\\":" | head -1'
+        for image_name in images.list_imgs_full_name():
+            if 'sadc' in image_name:
+                release_line = utils.run(cmd % image_name, timeout=30)
+                rst = re.search(self.regx, release_line.stdout)
+                if rst is not None:
+                    if str(self.config['rls_ver']) == rst.group():
+                        self.sub_stuff['img_name'] = image_name
+                        self.loginfo('Image is loaded and match the version.')
+                        return True
+                else:
+                    raise ValueError('No digital release info found!')
+        return False
 
-    def postprocess(self):
-        self.failif_ne(self.sub_stuff['load_result'].exit_status, 0,
-                       'Fail to load image!')
+    def get_installed(self):
+        """ Have to be used after load_image """
+        cmd = 'sudo atomic install %s' % self.sub_stuff['img_name']
+        utils.run(cmd, timeout=120, ignore_status=True)
+        self.loginfo('Image is successfully installed')
+
+    def get_run(self):
+        cmd_ps = 'sudo docker ps --format={{.Names}}'
+        cmd_run = 'sudo atomic run %s' % self.sub_stuff['img_name']
+        self.get_installed()
+        if 'sadc' not in utils.run(cmd_ps).stdout:
+            utils.run(cmd_run, timeout=120)
+
+    def format_output(self, output):
+        """
+        Output of a linux command often includes tabs and spaces,
+        this method tries to convert output to python lists
+        """
+        converted = []
+        tmp = []
+        for eachLine in output.split('\n'):
+            tmp = re.split('\s\s+|\t+', eachLine)
+            # Sometimes returned list need to change to set
+            tmp = tuple(tmp)
+            converted.append(tmp)
+        return converted
 
 
-class install(SubSubtest):
+class install(sadc_base):
 
     def initialize(self):
         super(install, self).initialize()
@@ -61,14 +102,20 @@ class install(SubSubtest):
         self.sub_stuff['etc_sysconfig_sysstat_ioconf_exists'] = None
         self.sub_stuff['usr_local_bin_sysstat_sh_exists'] = None
 
+        self.load_image(self.config['img_stored_location'])
+        # Uninstall the image first, if there was some older version installed
+        try:
+            self.loginfo('Uninstall the image first')
+            utils.run('sudo atomic uninstall %s' % self.sub_stuff['img_name'],
+                      timeout=30)
+        except error.CmdError:
+            self.loginfo('Uninstall failed!')
+
     def run_once(self):
         super(install, self).run_once()
-        imgs = DockerImages(self)
-        for img_name in imgs.list_imgs_full_name():
-            if 'sadc' in img_name:
-                break
-        ins_result = utils.run('sudo atomic install %s' % img_name,
-                               timeout=60, ignore_status=True)
+        ins_result = utils.run(
+            'sudo atomic install %s' % self.sub_stuff['img_name'],
+            timeout=60, ignore_status=True)
         self.sub_stuff['install_result'] = ins_result
 
         # List /etc/cron.d/sysstat
@@ -97,19 +144,41 @@ class install(SubSubtest):
     def check_etc_crond_sysstat(self):
         self.failif_ne(self.sub_stuff['etc_crond_sysstat_exists'], 0,
                        '/etc/cron.d/sysstat does not exist on host!')
+        self.loginfo('/etc/cron.d/sysstat is created')
 
     def check_etc_sysconfig_sysstat(self):
         self.failif_ne(self.sub_stuff['etc_sysconfig_sysstat_exists'], 0,
                        '/etc/sysconfig/sysstat does not exist on host!')
+        self.loginfo('/etc/sysconfig/sysstat is created')
 
     def check_etc_sysconfig_sysstat_ioconf(self):
         self.failif_ne(
             self.sub_stuff['etc_sysconfig_sysstat_ioconf_exists'], 0,
             '/etc/sysconfig/sysstat.ioconf does not exist on host!')
+        self.loginfo('/etc/sysconfig/sysstat.ioconf is created')
 
     def check_usr_local_bin_sysstat_sh(self):
         self.failif_ne(self.sub_stuff['usr_local_bin_sysstat_sh_exists'], 0,
                        '/usr/local/bin/sysstat.sh does not exist on host!')
+        self.loginfo('/usr/local/bin/sysstat.sh is created')
+
+    def check_size(self):
+        """
+        This method is to check image's size.
+        The correct size must be between 90%-110% of 208 MB.
+        """
+        can_pass = 0
+        min_size = 0.9 * 208
+        max_size = 1.1 * 208
+        cmd = "sudo docker images --format={{.Repository}}:{{.Tag}}\
+hope-not-exist{{.Size}} | grep %s" % self.sub_stuff['img_name']
+        rst = utils.run(cmd).stdout.split('hope-not-exist')[-1]
+        actual_size = float(rst[:-3])
+        self.loginfo('Image size is %s' % actual_size)
+        self.loginfo('Greater than %f, lower than %f' % (min_size, max_size))
+        if min_size < actual_size < max_size:
+            can_pass = 1
+        self.failif_ne(can_pass, 1, "Size is not Correct")
 
     def postprocess(self):
         super(install, self).postprocess()
@@ -119,3 +188,4 @@ class install(SubSubtest):
         self.check_etc_sysconfig_sysstat()
         self.check_etc_sysconfig_sysstat_ioconf()
         self.check_usr_local_bin_sysstat_sh()
+        self.check_size()
